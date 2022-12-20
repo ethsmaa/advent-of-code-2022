706ec7434e9b1b1194a804e14867e6a79db8cf71
esma <esmaoruccs@gmail.com>
2022-12-20 21:22:47 +0300

    day1
    find the calories

---
+++ day1/day1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+string[] lines = File.ReadAllLines("day1.txt");
+int[] sumArray = new int[500];
+int sum = 0;
+int count = -1;
+for (int i = 0; i < lines.Length; i++)
+{
+    if (lines[i] != "")
+        sum += Convert.ToInt32(lines[i]);
+
+    if (lines[i] == "")
+    {
+        count++;
+        sumArray[count] = sum;
+        sum = 0;
+    }
+}
+Array.Sort(sumArray);
+Array.Reverse(sumArray);
+Console.WriteLine(sumArray[0]); // the elf with the most calories
+Console.WriteLine(sumArray[0] + sumArray[1] + sumArray[2]); // sum of the calories
