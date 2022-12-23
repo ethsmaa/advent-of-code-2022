36e49be762b01f44f744d8038525f8dbade7459d
esma <esmaoruccs@gmail.com>
2022-12-23 21:56:38 +0300

    day2 eklendi

--- day2/day2.cs
+++ day2/day2.cs
@@ -7,13 +7,16 @@ int finalTotal = 0;
 
 for (int i = 0; i < file.Length; i++)
 {
+
     char yourshape = file[i][2];
     char elveshape = file[i][0];
     switch (elveshape)
     {
         case 'A': // tas
+
             switch (yourshape)
             {
+
                 case 'X':
                     won += 3;
                     shape += 1;
@@ -29,13 +32,20 @@ for (int i = 0; i < file.Length; i++)
                     shape += 3;
                     break;
             }
+
             break;
+
+
+
         case 'B': //  kagıt
+
             switch (yourshape)
             {
+
                 case 'X':
                     shape += 1;
                     break;
+
                 case 'Y':
                     won += 3;
                     shape += 2;
@@ -46,8 +56,12 @@ for (int i = 0; i < file.Length; i++)
                     shape += 3;
                     break;
             }
+
             break;
+
+
         case 'C': // makas
+
             switch (yourshape)
             {
 
@@ -96,51 +110,73 @@ for (int i = 0; i < file.Length; i++)
                     won += 0;
                     shape += 3;
                     break;
+
                 case 'Y': //tie /a
                     won += 3;
                     shape += 1;
                     break;
+
                 case 'Z': //win /b
                     won += 6;
                     shape += 2;
                     break;
             }
+
+
             break;
+
+
         case 'B': // kagıt
+
             switch (yourshape)
             {
+
                 case 'X': //lose // a
                     won += 0;
                     shape += 1;
                     break;
+
                 case 'Y': //tie /b
                     won += 3;
                     shape += 2;
                     break;
+
                 case 'Z': //win /c
                     won += 6;
                     shape += 3;
                     break;
             }
+
+
             break;
+
         case 'C':
+
+
             switch (yourshape)
             {
+
                 case 'X': //lose // b
                     won += 0;
                     shape += 2;
                     break;
+
                 case 'Y': //tie /c
                     won += 3;
                     shape += 3;
                     break;
+
                 case 'Z': //win /a
                     won += 6;
                     shape += 1;
                     break;
             }
+
             break;
+
     }
+
     finalTotal = won + shape;
 }
+
 Console.WriteLine(finalTotal);
